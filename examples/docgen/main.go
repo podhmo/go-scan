f4d8776fa120544de22d1683fb91543212aa9052
podhmo <ababjam61+github@gmail.com>
2025-08-22 16:14:58 +0900

    Update examples/docgen/main.go

--- examples/docgen/main.go
+++ examples/docgen/main.go
@@ -24,7 +24,7 @@ func main() {
 	flag.StringVar(&patternsFile, "patterns", "", "Path to a Go file with custom pattern configurations")
 	flag.Parse()
 
-	logLevel := slog.LevelWarn
+	logLevel := slog.LevelInfo
 	if debug {
 		logLevel = slog.LevelDebug
 	}
